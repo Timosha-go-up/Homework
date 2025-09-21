61f498655a2afe4b579dd668311e251d9b96026a
Roman Ochagow <ohagow.r@gmail.com>
2025-09-21 22:26:23 +0500

    Task 15.6.6

---
+++ Task-16-6-6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_16_6_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
